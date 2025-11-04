44ff9001b63a232381a900fc37b43ace57f6fa10
emmanuel macharia <samuelmarsha@outlook.com>
2025-11-04 19:44:25 +0300

    added eslint rule to be ignorred

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,7 +4,8 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ["@nuxt/eslint", "@nuxt/fonts", "@nuxt/icon", "@nuxt/image"],
   css: ["./app/assets/css/main.css"],
+  // vite plugin for tailwindcss has a type issue; so we cast it to any
   vite: {
     plugins: [tailwindcss()],
-  } as any,
+  } as any, // eslint-disable-line @typescript-eslint/no-explicit-any
 });

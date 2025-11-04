1938a686cac9e4e2a6fde5b1d75a0b998e7be2f1
emmanuel macharia <samuelmarsha@outlook.com>
2025-11-04 19:51:25 +0300

    tailwind deps now dev deps

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="flex flex-col items-center justify-center min-h-screen py-2">
-        <p class="font-bold text-green-800 text-3xl"> Hello Vue
+        <p class="font-bold text-green-600 text-3xl"> Hello Vue
         </p>
     </div>
 </template>

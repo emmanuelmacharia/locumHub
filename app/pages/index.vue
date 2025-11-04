dba2607e30c7d195eae17df1839a02290c6c8c3f
emmanuel macharia <samuelmarsha@outlook.com>
2025-11-04 20:26:51 +0300

    we now have shadcn set up successfully

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -2,5 +2,8 @@
     <div class="flex flex-col items-center justify-center min-h-screen py-2">
         <p class="font-bold text-green-600 text-3xl"> Hello Vue
         </p>
+        <div>
+            <UiButton variant="secondary">Click me</UiButton>
+        </div>
     </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,10 +2,27 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
-  modules: ["@nuxt/eslint", "@nuxt/fonts", "@nuxt/icon", "@nuxt/image"],
+  modules: [
+    "@nuxt/eslint",
+    "@nuxt/fonts",
+    "@nuxt/icon",
+    "@nuxt/image",
+    "shadcn-nuxt",
+  ],
   css: ["./app/assets/css/main.css"],
   // vite plugin for tailwindcss has a type issue; so we cast it to any
   vite: {
     plugins: [tailwindcss()],
   } as any, // eslint-disable-line @typescript-eslint/no-explicit-any
+  shadcn: {
+    /**
+     * Prefix for all the imported component
+     */
+    prefix: "",
+    /**
+     * Directory that the component lives in.
+     * @default "./components/ui"
+     */
+    componentDir: "./components/ui",
+  },
 });

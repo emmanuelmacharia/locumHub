e73cb5cbc722a4f3da15d266ee759cfba5932498
emmanuel macharia <samuelmarsha@outlook.com>
2025-11-04 19:38:39 +0300

    tailwind is installed and now working

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,7 +1,6 @@
 <template>
     <div class="flex flex-col items-center justify-center min-h-screen py-2">
-        <p class="text-bold text-green-500 text-3xl">
-            Hello Vue
+        <p class="font-bold text-green-800 text-3xl"> Hello Vue
         </p>
     </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,10 @@
+import tailwindcss from "@tailwindcss/vite";
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
-  modules: [
-    "@nuxt/eslint",
-    "@nuxt/fonts",
-    "@nuxt/icon",
-    "@nuxt/image",
-    "@nuxtjs/tailwindcss",
-  ],
+  modules: ["@nuxt/eslint", "@nuxt/fonts", "@nuxt/icon", "@nuxt/image"],
+  css: ["./app/assets/css/main.css"],
+  vite: {
+    plugins: [tailwindcss()],
+  } as any,
 });

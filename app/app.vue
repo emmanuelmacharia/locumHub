edf08a7123edd0f4122435e3d6d1cafd282dd9b2
emmanuel macharia <samuelmarsha@outlook.com>
2026-01-07 13:01:59 +0300

    added sonner and wired it up

--- app/app.vue
+++ app/app.vue
@@ -1,5 +1,8 @@
 <template>
   <div class="min-h-screen bg-background text-foreground">
+    <ClientOnly>
+      <UIToaster position="top-center" rich-colors />
+    </ClientOnly>
     <NuxtLayout>
       <NuxtPage />
     </NuxtLayout>

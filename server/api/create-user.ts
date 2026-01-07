d879370682812351791d5aa29b77b554264ff402
emmanuel macharia <samuelmarsha@outlook.com>
2026-01-07 13:09:49 +0300

    implements the create user api

--- server/api/create-user.ts
+++ server/api/create-user.ts
@@ -1,4 +1,54 @@
-export default defineEventHandler((event) => {
-  console.log(event);
-  return "Hello create-user";
+import { api } from "~~/convex/_generated/api";
+import { userPayloadSchema } from "../../app/utils/types/onboardingPayloads";
+import { getConvexClient } from "../utils/convex";
+import { isAppError } from "../utils/convexErrors";
+
+export default defineEventHandler(async (event) => {
+  const result = await readValidatedBody(event, (body: unknown) =>
+    userPayloadSchema.safeParse(body)
+  );
+
+  if (!result.success) {
+    throw createError({
+      statusCode: 400,
+      statusMessage: "Validation Error",
+      data: result.error.issues,
+    });
+  }
+
+  const convex = getConvexClient();
+  try {
+    const userId = await convex.mutation(api.user.createUser, {
+      ...result.data,
+      phoneNumber: result.data.phoneNumber ?? undefined,
+      clerkCreatedAt: new Date(result.data.clerkCreatedAt).getTime(),
+      clerkUpdatedAt: new Date(result.data.clerkUpdatedAt).getTime(),
+    });
+
+    return {
+      statusCode: 201,
+      statusMessage: "User successfully created",
+      data: {
+        userId,
+      },
+    };
+  } catch (error) {
+    if (isAppError(error)) {
+      const { data } = error;
+      return createError({
+        statusCode: data.statusCode,
+        statusMessage: data.statusMessage,
+        data: {
+          code: data.code,
+        },
+      });
+    } else {
+      // handle other error types
+      return createError({
+        statusCode: 500,
+        statusMessage: "Something went wrong; Please try again later",
+        data: error,
+      });
+    }
+  }
 });

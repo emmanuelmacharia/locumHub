91b5194eccb9e0d210408b0c0a08614b4eaf9cac
emmanuel macharia <samuelmarsha@outlook.com>
2026-01-12 15:02:09 +0300

    find work cannot be implemented until the actual work item (shift) is created

---
+++ app/pages/findWork.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+// import { useAuthenticatedUser } from "~/composables/useAuthenticatedUser";
+
+definePageMeta({
+  layout: "landing-page",
+});
+
+/**
+ *
+ *   get user details => if user is fully onboarded (clerk + convex) => show full details
+ *
+ */
+
+// const {
+//   user,
+//   userData,
+//   isAuthenticated,
+//   isLoaded,
+//   isLoading,
+//   error,
+//   isClerkLoaded,
+//   isPending,
+// } = useAuthenticatedUser();
+
+// TODO: TO BE IMPLEMENTED WHEN WE CAN CREATE SHIFTS; RIGHT NOW, WE REALLY CANT DO ANYTHING HERE
+</script>
+
+<template>
+  <div>Page: findWork</div>
+</template>
+
+<style scoped></style>

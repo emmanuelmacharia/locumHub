010658fcdaa6a103126d6bf90d4aa40124ba1b7d
emmanuel macharia <samuelmarsha@outlook.com>
2026-01-30 18:02:09 +0300

    api for fetching user and userprofile

---
+++ server/api/auth/getUserProfile.ts
@@ -0,0 +1,22 @@
+import { api } from "~~/convex/_generated/api";
+
+export default defineEventHandler(async (event) => {
+  const { isAuthenticated, userId } = await verifyAuth(event);
+
+  if (!isAuthenticated || !userId) return null;
+
+  const convex = getConvexClient();
+
+  try {
+    const userProfile = await convex.query(
+      api.users.userProfile.getUserProfileByClerkId,
+      { clerkUserId: userId },
+    );
+
+    return userProfile || null;
+  } catch (e) {
+    // eslint-disable-next-line no-console
+    console.log(e);
+    return null;
+  }
+});
